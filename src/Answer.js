129ad0c796155e4aa3a403c8fa17db6381e51730
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2019-02-27 13:50:33 -0700

    Stop grabbing virtual DOM elements, instead use state

--- src/Answer.js
+++ src/Answer.js
@@ -5,6 +5,11 @@ import soundy from './assets/Blop.mp3';
 class Answer extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      classes: '',
+      answer: this.props.answer,
+      key: this.props.key
+    }
   }
 
   checkAnswer = (e) => {
@@ -13,15 +18,21 @@ class Answer extends Component {
     let button1;
     let button2;
     let audio;
-    if(e.target.innerHTML !== this.props.correct) {
-      e.target.classList.add("incorrect")
+    if(this.props.answer !== this.props.correct) {
+      this.setState({
+        classes: 'incorrect'
+      })
+      console.log(this.state.answer)
+      console.log(this.state)
       toastMssg = 'Sorry!!! noooooo 😞'
       toastDesc = 'Move on to the next question.'
       button1 = 'Move On'
       button2 = 'Try Again'
       audio = new Audio(soundfile)
     } else {
-      e.target.classList.add("correct")
+      this.setState({
+        classes: 'correct'
+      })
       toastMssg = 'Correct!';
       toastDesc = 'You are on your way to learning code! Continue to the next question. 😊'
       button1 = 'Continue'
@@ -30,13 +41,19 @@ class Answer extends Component {
     }
       audio.play();
       this.props.toastLaunch(toastMssg, toastDesc, button1, button2)
+      this.resetState();
   }
 
+  resetState = () => {
+    this.setState({
+      classes: ''
+    })
+  }
 
 
   render() {
     return (
-      <div className="Answer" onClick={this.checkAnswer}>
+      <div className={this.state.classes} className="Answer" onClick={this.checkAnswer}>
         {this.props.answer}
       </div>
     )
